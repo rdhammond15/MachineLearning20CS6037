b3f02ad9b92d2ca1a75adef3b4c5566348cdd7b8
Richard Hammond <rdhammond15@gmail.com>
2020-10-23 15:08:48 -0400

    Add remaining equations

--- SMO.py
+++ SMO.py
@@ -6,7 +6,7 @@
 import numpy as np
 
 class Smo(object):
-    def __init__(self, x, y):
+    def __init__(self, x, y, epsilon):
         """
         Initialize the algorthim with the training data
         
@@ -17,6 +17,7 @@ class Smo(object):
         self.alpha = self.init_alpha()
         self.w = self.calc_weight()
         self.b = 0
+        self.epsilon = epsilon
 
     def init_alpha(self):
         """
@@ -54,5 +55,72 @@ class Smo(object):
         
         @return dot product of xi * xj
         """
-        return self.x[i].dot(self.x[j])
+        return np.dot(self.x[i], self.x[j])
+        
+    def error(self, i):
+        """
+        Claculate difference from label
+        
+        @param i: current index
+        """
+        sum = 0 
+        for j in self.size:
+            sum += self.aplpha[j] * self.y[j] * (self.kernel(self.i1, j) - self.kernel(i, j) +
+                self.y[i] - self.y[self.i1]
+        return sum
+        
+    def kkt(self, i):
+        """
+        Calculate KKT condition
+        
+        @param i: current index
+        """
+        return self.alpha[i] * (self.y[i] * (np.dot(self.w, self.x[i]) + self.b) - 1)
+        
+    def calc_i1(self):
+        """
+        Find argmax of KKT condition
+        
+        @return the max index
+        """
+        return np.argmax([self.kkt(i) for i in range(self.size)])
+        
+    def calc_i2(self):
+        """
+        Find armgax of error
+        
+        @return the max index
+        """
+        return np.argmax([self.error(i) for i in range(self.size)])
+        
+    def update_alpha2(self):
+        """
+        Update the value of alpha 2
+        """
+        self.alpha2_old = self.alpha[self.i2]
+        self.alpha[self.i2] = self.alpha[self.i2] + (self.y[self.i2] * self.error(self.i2))/self.k
+        
+    def update_alpha1(self):
+        """
+        Update the value of alhpa1 based on alpha 2
+        """
+        self.alpha[self.i1] = self.alpha[self.i1] + self.y[self.i1] * self.y[self.i2] * (self.alpha2_old - self.alpha[self.i2])
+        
+    @property
+    def k(self):
+        return self.kernel(self.i1, self.i1) + kernel(self.i2, self.i2) - 2*kerenl(self.i1, self.i2)
+    
+    def run(self):
+        """
+        Execute the SMO
+        """
+        self.i1 = self.calc_i1()
+        self.i2 = self.calc_i2()
+        
+        # ai < epsilon, a1 <-- 0
+        self.alpha = map(lambda x: x if x >= self.epsilon else 0, self.alpha)
+        
+        # TODO select ai > 0, calculate b
+        
+        # TODO test for classification and repeat until classified
         
