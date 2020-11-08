d4e675e6226baabecc4bd675347235862dd4ce88
Richard Hammond <rdhammond15@gmail.com>
2020-11-08 15:29:38 -0500

    Use a range of bins with id3

--- Assignment3/id3.py
+++ Assignment3/id3.py
@@ -33,7 +33,16 @@ class ID3(object):
         @return True if flower other than setosa or False if setosa
         """
         branch = instance[node.name]
-        next_node = node.branches[branch]
+
+        try:
+            next_node = node.branches[branch]
+        # data is binned too much and the training data is missing one of the values
+        except KeyError:
+            for key in node.branches.iterkeys():
+                # round up to next key
+                if key > branch:
+                    next_node = node.branches[key]
+                    break
 
         if not isinstance(next_node, Node):
             return next_node
@@ -150,26 +159,28 @@ if __name__ == "__main__":
     # get training and test sets
     prop_train, prop_test, label_train, label_test = train_test_split(iris.data, labels, test_size=.5, random_state=5)
 
-    # descretize the data
-    training_set = prop_train
-    discretizer = KBinsDiscretizer(n_bins=5, encode='ordinal', strategy='kmeans')
-    discretizer.fit(training_set) 
+    # train and test with different bins
+    for bin in range(5, 25, 5):
+        # descretize the data
+        training_set = prop_train
+        discretizer = KBinsDiscretizer(n_bins=bin, encode='ordinal', strategy='kmeans')
+        discretizer.fit(training_set) 
 
-    # create panda out of features and labels
-    features = iris.feature_names
-    df_train = pandas.DataFrame(discretizer.transform(training_set), columns=features)
-    df_train.insert(len(features), "labels", label_train, True)
+        # create panda out of features and labels
+        features = iris.feature_names
+        df_train = pandas.DataFrame(discretizer.transform(training_set), columns=features)
+        df_train.insert(len(features), "labels", label_train, True)
 
-    id3 = ID3(df_train)
-    print id3.tree
+        id3 = ID3(df_train)
+        print id3.tree
 
-    # validate the training set
-    discretizer.fit(prop_test)
-    df_test = pandas.DataFrame(discretizer.transform(prop_test), columns=features)
-    df_test.insert(len(features), "labels", label_test, True)
+        # validate the training set
+        discretizer.fit(prop_test)
+        df_test = pandas.DataFrame(discretizer.transform(prop_test), columns=features)
+        df_test.insert(len(features), "labels", label_test, True)
 
-    test_res = []
-    for row in df_test.iterrows():
-        test_res.append(id3.classify(id3.tree, row[1]) == row[1]['labels'])
+        test_res = []
+        for row in df_test.iterrows():
+            test_res.append(id3.classify(id3.tree, row[1]) == row[1]['labels'])
 
-    print "ID3 Classified {} out of {} instances correctly".format(test_res.count(1), len(test_res))
+        print "ID3 Classified {} out of {} instances correctly".format(test_res.count(1), len(test_res))
