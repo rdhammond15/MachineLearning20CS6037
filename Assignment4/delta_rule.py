b6b6a58f7647dbcc6354d875173786d9b2941884
Richard Hammond <rdhammond15@gmail.com>
2020-11-22 14:39:45 -0500

    Test over differnt learning rates

--- Assignment4/delta_rule.py
+++ Assignment4/delta_rule.py
@@ -18,11 +18,20 @@ class Delta(object):
         @param epoch: The number of iteratations to use for training
         @param rate: The learning rate eta
         """
-        self.data = train 
         self.labels = labels
+        self.data = train 
         self.w = np.random.rand(train.shape[1])
         self.epoch = epoch
         self.rate = rate
+        self.df = pandas.DataFrame(self.data, columns=['bias', 'x1', 'x2'])
+        self.df.insert(3, "labels", labels, True)
+
+    def load_test_data(self, test, labels):
+        """
+        Use after training so that the hypothesis can be used wth the test data
+        """
+        self.labels = labels
+        self.data = test
         self.df = pandas.DataFrame(self.data, columns=['bias', 'x1', 'x2'])
         self.df.insert(3, "labels", labels, True)
 
@@ -62,22 +71,42 @@ class Delta(object):
 
             # plot the decision boundry peridocially
             if i in (5, 10, 50, 100):
-                plt.figure(i)
-                x_data = np.linspace(0, 1, 100)
-                x_in = [[x] for x in x_data]
-                x_axis = np.asarray(x_in)
-                x_axis = np.concatenate((x_in, x_axis), axis=1)
-                x_axis = np.concatenate((np.ones((x_axis.shape[0], 1)), x_axis), axis=1)
-                y_axis = x_axis.dot(self.w)
-                plt.plot(x_data, y_axis, 'b-')
+                self.plot_decision_surface(i)
 
-                # split up the data based on label and plot where it lies
-                positive = self.df.loc[self.df['labels'] == 1]
-                negative = self.df.loc[self.df['labels'] == -1]
-                plt.plot(positive["x2"].values, positive["x1"].values, "r+")
-                plt.plot(negative["x2"].values, negative["x1"].values, "y_")
-                plt.title("Decision Boundry at Epoch %d" % i)       
+        plt.figure(1)
+        plt.ylabel('Error')
+        plt.xlabel('Epoch')
+        plt.title("Error per Epoch")
+        plt.show()
 
+    def plot_decision_surface(self, plot_num, title="Train Data"):
+        """
+        Plot the hypothesis line along with where the data actually lands
+
+        @param plot_num: The plot number to use
+        @param mode: Title prefix for the plot
+        """
+        plt.figure(plot_num)
+        x_data = np.linspace(0, 1, 100)
+        x_in = [[x] for x in x_data]
+        x_axis = np.asarray(x_in)
+        x_axis = np.concatenate((x_in, x_axis), axis=1)
+        x_axis = np.concatenate((np.ones((x_axis.shape[0], 1)), x_axis), axis=1)
+        y_axis = x_axis.dot(self.w)
+        plt.plot(x_data, y_axis, 'b-')
+
+        # split up the data based on label and plot where it lies
+        positive = self.df.loc[self.df['labels'] == 1]
+        negative = self.df.loc[self.df['labels'] == -1]
+        plt.plot(positive["x2"].values, positive["x1"].values, "r+")
+        plt.plot(negative["x2"].values, negative["x1"].values, "y_")
+        suffix = "at Epoch %d" % plot_num if title == "Train Data" else ""
+        plt.title(title + " Decision Boundry " + suffix)       
+
+    def test(self):
+        """
+        Use the trained weight vector on a new set of data
+        """
         # show accuracy stats
         correct = 0
         for predicted, y in zip(self.predict, self.labels):
@@ -85,11 +114,7 @@ class Delta(object):
                 correct +=1 
         
         print "Predicted %d correctly out of %d" % (correct, self.labels.shape[0])
-        
-        plt.figure(1)
-        plt.ylabel('Error')
-        plt.xlabel('Epoch')
-        plt.title("Error per Epoch")
+        self.plot_decision_surface(1, "Test Data")
         plt.show()
     
     def cost(self):
@@ -99,7 +124,6 @@ class Delta(object):
         @return the delta change for the weights
         """
         return self.rate * np.dot((self.labels - np.sign(self.hypothesis)), self.data)
-
 
 
 def concept(x1, x2):
@@ -128,5 +152,9 @@ if __name__ == "__main__":
     training = np.concatenate((np.ones((100, 1)), training), axis=1)
     test = np.concatenate((np.ones((100, 1)), test), axis=1)
 
-    delta_rule = Delta(training, y_train, 100, 0.1)
-    delta_rule.train()
+    for rate in (0.5, 0.1, 0.01, 0.001, 0.0001):
+        print "Training with learning rate {}".format(rate)
+        delta_rule = Delta(training, y_train, 100, rate)
+        delta_rule.train()
+        delta_rule.load_test_data(test, y_test)
+        delta_rule.test()
